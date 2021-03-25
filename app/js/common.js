11732f32e998318214de005e7765b61ae182955b
stirlitz2322 <33348510+stirlitz2322@users.noreply.github.com>
2021-03-25 19:55:13 +0200

    Partners

--- app/js/common.js
+++ app/js/common.js
@@ -73,6 +73,28 @@ $(function() {
 		nav: false,
 		autoHeight: true
 	});
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200 : {
+				items: 4
+			}
+		}
+	});
 
 	//E-mail Ajax Send
 	$("form.callback").submit(function() { //Change
