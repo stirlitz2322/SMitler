9e6055ac0f13408fdac3b6d72292c5556922f224
stirlitz2322 <33348510+stirlitz2322@users.noreply.github.com>
2021-03-27 20:07:53 +0200

    footer and preloader

--- app/js/common.js
+++ app/js/common.js
@@ -95,7 +95,17 @@ $(function() {
 			}
 		}
 	});
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		}else {
+			$('.top').removeClass('active');
+		}
 
+	});
+	$('.top').click(function(){
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing')
+	});
 	//E-mail Ajax Send
 	$("form.callback").submit(function() { //Change
 		var th = $(this);
@@ -118,5 +128,9 @@ $(function() {
 		$('.carousel-services-content').equalHeights();
 	}onResize();
 	window.onresize = function() {onResize()};
+
+});
+$(window).on('load', function(){
+	$('.preloader').delay(1000).fadeOut('slow')
 
 });
